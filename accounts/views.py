feb9fb77b9e23046b8f25bd6bb046fe7eae10228
medmoe <med.seffah@gmail.com>
2024-06-28 13:58:07 -0400

    added refresh token endpoint

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from rest_framework import permissions, status
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .serializers import UserProfileSerializer, CustomTokenObtainPairSerializer
 
@@ -51,3 +53,21 @@ class CustomTokenObtainPairView(TokenObtainPairView):
         response.data.pop('refresh')
         response.data.pop('access')
         return response
+
+
+class CustomTokenRefreshView(TokenRefreshView):
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        refresh_token = request.COOKIES.get('refresh', None)
+        if refresh_token is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.get_serializer(data={'refresh': refresh_token})
+        try:
+            serializer.is_valid(raise_exception=True)
+            response = Response(serializer.validated_data, status=status.HTTP_200_OK)
+            response.set_cookie(key='access', value=response.data['access'], httponly=True, samesite='Lax')
+            return response
+        except TokenError as e:
+            return Response({'detail': str(e)}, status=status.HTTP_401_UNAUTHORIZED)
+        except Exception as e:
+            return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
