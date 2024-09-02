6671435f5dee6f588e11133ff0da77e8fab3da7f
medmoe <med.seffah@gmail.com>
2024-09-01 21:22:27 -0400

    updated validation

--- drivers/serializers.py
+++ drivers/serializers.py
@@ -15,8 +15,12 @@ class DriverSerializer(serializers.ModelSerializer):
 
     def validate(self, attrs):
         data = super().validate(attrs)
-        fields_to_validate = ['license_number', 'phone_number', 'email', 'license_expiry_date', 'date_of_birth', 'hire_date']
-        for field in fields_to_validate:
+        for field in ['license_expiry_date', 'date_of_birth', 'hire_date']:
+            field_value = data.get(field, None)
+            if field_value is None:
+                raise serializers.ValidationError(f"{field.replace('_', ' ').capitalize()} can't be empty")
+
+        for field in ['license_number', 'phone_number', 'email']:
             field_value = data.get(field, None)
             if field_value is None and field != 'email':
                 raise serializers.ValidationError(f"{field.replace('_', ' ').capitalize()} can't be empty")
@@ -25,7 +29,3 @@ class DriverSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError(f"A driver with this {field.replace('_', ' ')} already exists")
 
         return data
-
-
-
-
