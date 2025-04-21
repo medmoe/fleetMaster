6bf923fb4c96f273ae9d15bc7fd4e53325290356
medse <med.seffah@gmail.com>
2025-04-21 18:10:32 -0400

    Add Facebook data deletion callback and workflow adjustments

--- fleetMaster/urls.py
+++ fleetMaster/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("accounts.urls")),
@@ -25,4 +24,6 @@ urlpatterns = [
     path('vehicles/', include("vehicles.urls")),
     path('maintenance/', include('maintenance.urls')),
     path('auth/', include("allauth.urls")),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
