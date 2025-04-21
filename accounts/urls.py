11df4d97101bb0d0877612a00755028c06de94b6
medse <med.seffah@gmail.com>
2025-04-21 16:12:38 -0400

    Add Facebook data deletion callback and workflow adjustments

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import SignUpView, LogoutView, CustomTokenObtainPairView, TokenVerificationView
+from .views import SignUpView, LogoutView, CustomTokenObtainPairView, TokenVerificationView, FacebookDataDeletionView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('login/', CustomTokenObtainPairView.as_view(), name='login'),
     path('refresh/', TokenVerificationView.as_view(), name='verify_token'),
+    path('facebook-data-deletion/', FacebookDataDeletionView.as_view(), name='fb-data-deletion'),
 ]
