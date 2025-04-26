bf63fb8f650eb58acd2b8e7f6b371542f14be1fc
medse <med.seffah@gmail.com>
2025-04-26 19:41:07 -0400

    extended access token lifetime

--- fleetMaster/settings.py
+++ fleetMaster/settings.py
@@ -286,7 +286,7 @@ MEDIA_URL = '/media/'
 
 # JWT configuration
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
