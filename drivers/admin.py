8a6d050db46f74648b588cebbbc80800a7cf40ba
medse <med.seffah@gmail.com>
2025-06-08 09:58:04 -0400

    Register models in admin for maintenance, drivers, and vehicles apps.

--- drivers/admin.py
+++ drivers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Driver, DriverStartingShift
+
 # Register your models here.
+
+admin.site.register(Driver)
+admin.site.register(DriverStartingShift)
--- maintenance/admin.py
+++ maintenance/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import MaintenanceReport, Part, ServiceProvider, PartsProvider, PartPurchaseEvent, ServiceProviderEvent
+
 # Register your models here.
+
+admin.site.register(MaintenanceReport)
+admin.site.register(Part)
+admin.site.register(ServiceProvider)
+admin.site.register(PartsProvider)
+admin.site.register(PartPurchaseEvent)
+admin.site.register(ServiceProviderEvent)
